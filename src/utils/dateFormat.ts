c288836c392fac4ca0c7140a4eb89d022c80e2e0
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2024-04-17 22:05:19 -0300

    feature: create ultils functions

---
+++ src/utils/dateFormat.ts
@@ -0,0 +1,7 @@
+export function formatDate(date: string) {
+  const dateObj = new Date(date)
+  const day = dateObj.getDate()
+  const month = dateObj.getMonth() + 1
+  const year = dateObj.getFullYear()
+  return `${day}/${month}/${year}`
+}
